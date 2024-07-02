00c25b9206a03a4c99044ca87cb7a1d678d9e095
Daniil Belinskyi <belinskijplay@gmail.com>
2024-07-02 19:07:12 +0300

    done file

--- prac/prac 1/main.c
+++ prac/prac 1/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-	printf("hello");
+int main(void)
+{
+	int t1, t2, t3;
+	double res;
+
+	printf("Input 3 values: ");
+	scanf_s("%d %d %d", &t1, &t2, &t3);
+
+	res = (double)(t1 + t2 + t3) / 3;
+
+	printf("\nTime required to eat the pie: %.2lf hours", res);
+
+	return 0;
 }
