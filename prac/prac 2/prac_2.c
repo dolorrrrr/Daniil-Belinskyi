569b0589bf47d959b6ee07af2856570270ce05a9
Daniil Belinskyi <belinskijplay@gmail.com>
2024-07-03 20:18:59 +0300

    1.1 final

--- prac/prac 2/prac_2.c
+++ prac/prac 2/prac_2.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
-#include <string.h>
 
 int gcd_two_nums(int a, int b)
 {
-    while(b != 0){
+    while (b != 0) {
         int temp = b;
         b = a % b;
         a = temp;
     }
-
     return a;
 }
 
 int lcm_two_nums(int a, int b)
 {
-    return (a * b)/gcd_two_nums(a, b);
+    return (a * b) / gcd_two_nums(a, b);
 }
-
 
 int main(void)
 {
     int size, lcm;
-    char nums[10] = {0};
     
     printf("Enter size: ");
     scanf("%d", &size);
-    for(int i = 0; i < size; i++){
+    
+    int nums[10];
+    printf("Enter numbers: ");
+    for (int i = 0; i < size; i++) {
         scanf("%d", &nums[i]);
     }
 
     lcm = nums[0];
-    for(int i = 0; i < size; i++){
-        lcm = gcd_two_nums(lcm, nums[i]);
+    for (int i = 1; i < size; i++) {
+        lcm = lcm_two_nums(lcm, nums[i]);
     }
 
-    printf("lcm: %d", lcm);
-
-    
+    printf("LCM: %d\n", lcm);
     
     return 0;
 }
