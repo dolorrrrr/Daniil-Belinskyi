ef6f80bbbe4bc557e9222ff180c65198407da9c0
Daniil Belinskyi <belinskijplay@gmail.com>
2024-07-03 20:02:29 +0300

    1.0 final prac_2

--- prac/prac 2/prac_2.c
+++ prac/prac 2/prac_2.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+int gcd_two_nums(int a, int b)
+{
+    while(b != 0){
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
 
 int main(void)
 {
-    int num;
-    printf("Enter a num: ");
-    scanf("%d", &num);
-    printf("%d", num);
+    int size, gcd;
+    char nums[10] = {0};
+    
+    printf("Enter size: ");
+    scanf("%d", &size);
+    for(int i = 0; i < size; i++){
+        scanf("%d", &nums[i]);
+    }
 
+    gcd = nums[0];
+    for(int i = 0; i < size; i++){
+        gcd = gcd_two_nums(gcd, nums[i]);
+    }
+
+    printf("GCD: %d", gcd);
+
+    
+    
     return 0;
 }
