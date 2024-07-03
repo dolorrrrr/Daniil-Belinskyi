6c83e370c6acf4a1715f663562f55e2974d4229e
Daniil Belinskyi <belinskijplay@gmail.com>
2024-07-03 21:59:29 +0300

    aplha

---
+++ prac/prac7/prac7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    int x1, y1, r1, x2, y2, r2;
+    scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+    printf("%d %d %d %d %d %d", x1, y1, r1, x2, y2, r2);
+}
