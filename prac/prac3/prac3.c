db0e13b8b9237db5bee3f8a20a719fa0c9b7cd86
Daniil Belinskyi <belinskijplay@gmail.com>
2024-07-07 22:50:13 +0300

    1.0

--- prac/prac3/prac3.c
+++ prac/prac3/prac3.c
@@ -9,8 +9,30 @@
 
 int main(void)
 {
-    int p;
+    int p, res;
+    printf("Input p: ");
     scanf("%d", &p);
+
+    int end5[31], end55[31], end9[31], end99[31];
+    
+    end5[1] = 1;
+    end55[1] = 0;
+    end9[1] = 1;
+    end99[1] = 0;
+    
+    if(p == 1){
+        printf("Result is: 2");
+        return 0;
+    }
+
+    for(int i = 2; i <= p; i++){
+        end5[i] = end9[i - 1] + end99[i - 1];
+        end55[i] = end5 [i - 1];
+        end9[i] = end5[i - 1] + end55[i - 1];
+        end99[i] = end9[i - 1];
+    }
+    res = end5[p] + end55[p] + end9[p] + end99[p];
+    printf("Result is: %d", res);
 
     return 0;
 }
